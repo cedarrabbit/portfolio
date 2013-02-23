89acf08c410fef363d11df64b507712b9722afae
Joshua Daniel <joshua.daniel@me.com>
2013-02-22 16:18:58 -0800

    saving

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,27 +5,27 @@ joshuaApp.controller('MainCtrl', function($scope, $timeout) {
 	var isotope_container = $('#isotope_container');
 
 	$scope.projects = [
-		{id:'p1',date:'2012',platform:'iOS',format:'Mobile App',client:'Accenture'},
-		{id:'p2',date:'2012',platform:'Android',format:'Mobile App',client:'Barclays'},
-		{id:'p3',date:'2011',platform:'iOS',format:'Mobile App',client:'FuelQuest'},
-		{id:'p4',date:'2010',platform:'HTML5',format:'Web App',client:'FINRA'},
-		{id:'p5',date:'2009',platform:'iOS',format:'Mobile App',client:'McGraw-Hill'},
-		{id:'p6',pdate:'2009',latform:'Android',format:'Mobile App',client:'McDonalds'},
-		{id:'p7',date:'2009',platform:'Air',format:'Desktop App',client:'NASA'},
-		{id:'p8',date:'2008',platform:'Flash',format:'Website',client:'Huawei'},
-		{id:'p8',date:'2008',platform:'Flex',format:'Web App',client:'Huawei'},
-		{id:'p8',date:'2008',platform:'Director',format:'CDROM',client:'Parrimark'},
-		{id:'p8',date:'2007',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2006',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2005',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2004',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2003',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2002',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2001',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'2000',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'1999',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'1998',platform:'iOS',format:'Mobile App',client:'Huawei'},
-		{id:'p8',date:'1997',platform:'iOS',format:'Mobile App',client:'Huawei'}
+		{id:'p1',date:'2012',platform:'iOS',format:'Mobile App',client:'Accenture',name:'Red Carpet'},
+		{id:'p2',date:'2012',platform:'Android',format:'Mobile App',client:'Barclays',name:'On The Go'},
+		{id:'p3',date:'2011',platform:'iOS',format:'Mobile App',client:'FuelQuest',name:'SMB Cloud Service'},
+		{id:'p4',date:'2010',platform:'HTML5',format:'Web App',client:'FINRA',name:'Nomad'},
+		{id:'p5',date:'2009',platform:'iOS',format:'Mobile App',client:'McGraw-Hill',name:'Paris'},
+		{id:'p6',pdate:'2009',latform:'Android',format:'Mobile App',client:'McDonalds',name:'Bumbo Pum Pum'},
+		{id:'p7',date:'2009',platform:'Air',format:'Desktop App',client:'NASA',name:'Lick A Shot'},
+		{id:'p8',date:'2008',platform:'Flash',format:'Website',client:'Huawei',name:'Willy Dilly'},
+		{id:'p8',date:'2008',platform:'Flex',format:'Web App',client:'Huawei',name:'Oingo Fucko'},
+		{id:'p8',date:'2008',platform:'Director',format:'CDROM',client:'Parrimark',name:'Wham Bam'},
+		{id:'p8',date:'2007',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Erectile Projectile'},
+		{id:'p8',date:'2006',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Electric Boogaloo'},
+		{id:'p8',date:'2005',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Stinky Pinky'},
+		{id:'p8',date:'2004',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Black Mamba'},
+		{id:'p8',date:'2003',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Operation Succeed'},
+		{id:'p8',date:'2002',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Monster Mashup'},
+		{id:'p8',date:'2001',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Virtual Poonani'},
+		{id:'p8',date:'2000',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Smashing Dumplings'},
+		{id:'p8',date:'1999',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Xpatti Extravaganza'},
+		{id:'p8',date:'1998',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Humping Monkeys'},
+		{id:'p8',date:'1997',platform:'iOS',format:'Mobile App',client:'Huawei',name:'Yellow Submarine'}
 	];
 
 	$scope.filtering = false;
--- app/scripts/directives/menu.js
+++ app/scripts/directives/menu.js
@@ -5,31 +5,33 @@ joshuaApp.directive('menu', function() {
 	return {
 		restrict: 'A',
 		link: function postLink(scope, element, attrs) {
-			var el = $(element);
+			console.log($('.dropdown li').length);
+			scope.test = 10;
+			// var el = $(element);
 
-			el.on('click', function(){
-				// close any other opened menus
-				$('.dropdown li').css('display','none');
+			// el.on('click', function(){
+			// 	// close any other opened menus
+			// 	$('.dropdown li').css('display','none');
 
-				// if the menu is closed by the user clicking outside an open menu
-				// the opened state here is out of context
-				if(!scope.filtering && opened != false)
-					opened = false;
+			// 	// if the menu is closed by the user clicking outside an open menu
+			// 	// the opened state here is out of context
+			// 	if(!scope.filtering && opened != false)
+			// 		opened = false;
 
-				opened = opened == el ? false : el;
+			// 	opened = opened == el ? false : el;
 
-				scope.filtering = opened == false ? false : true;
-				scope.$apply();
+			// 	scope.filtering = opened == false ? false : true;
+			// 	scope.$apply();
 
-				el.siblings().each(function(index,el){
-					if(!opened)
-					{
-						$(el).css('display','none');
-					} else {
-						$(el).delay(index*10).fadeIn(100);
-					}
-				})
-			})
+			// 	el.siblings().each(function(index,el){
+			// 		if(!opened)
+			// 		{
+			// 			$(el).css('display','none');
+			// 		} else {
+			// 			$(el).delay(index*10).fadeIn(100);
+			// 		}
+			// 	})
+			// })
 		}
 	};
 });
