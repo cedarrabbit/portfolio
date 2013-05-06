40f3c45d9e8edab2119ee84f3b2cf24bac54a59f
cedarrabbit <cedarrabbit@mac.com>
2013-05-05 18:17:51 -0700

    saving

--- app/scripts/directives/scrollMap.js
+++ app/scripts/directives/scrollMap.js
@@ -1,18 +1,28 @@
 'use strict';
 
-joshuaApp.directive('scrollMap', function() {
+joshuaApp.directive('scrollMap', function($timeout, model) {
   return {
     restrict: 'A',
     link: function postLink(scope, element, attrs) {
-    	var el = $(element);
-    	var tlHeight = el.parent().height();
+    	var el = $(element),
+            timelineHeight = el.parent().height(),
+            projectListHeight = $('.project_list')[0].clientHeight,
+            percentFromTop = 0;
+
+        $timeout(function(){
+            scope.$watch('model.projectsPercentViewable', function(){
+                el.css('height',model.projectsPercentViewable*timelineHeight);
+            })
+        })
+
+        window.onresize = function() {
+            timelineHeight = el.parent().height();
+            el.css('top',percentFromTop*timelineHeight);
+        }
 
     	scope.$watch('test', function(val){
-	    	var perc = val/100;
-    		console.log(perc)
-    		// scope.test = 100 * this.scrollTop / (this.scrollHeight-this.clientHeight);
-
-    		$(element).css('top',val*tlHeight);
+	    	percentFromTop = val/100;
+    		el.css('top',percentFromTop*timelineHeight);
     	})
     }
   };
--- app/scripts/directives/scrollSpy.js
+++ app/scripts/directives/scrollSpy.js
@@ -1,9 +1,15 @@
 'use strict';
 
-joshuaApp.directive('scrollSpy', function() {
+joshuaApp.directive('scrollSpy', function($timeout,model) {
   return {
     restrict: 'A',
     link: function postLink(scope, element, attrs) {
+    	
+    	$timeout(function(){
+    	  	// record the percent of the project list that is viewable
+	    	model.projectsPercentViewable = element[0].clientHeight/element[0].scrollHeight;
+    	})
+
 		$(element).scroll(function(){
 			scope.test = 100 * this.scrollTop / (this.scrollHeight-this.clientHeight);
 			scope.$apply();
